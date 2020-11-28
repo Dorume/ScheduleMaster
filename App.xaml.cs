4e9344fac541633310de57f555ff87e3e6d762e1
Савицкий Даня <savitskiydanulo@gmail.com>
2020-11-28 16:20:17 +0200

    Контейнер сервисов

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,13 +5,37 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace ScheduleMaster
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private static IHost _Host;
+
+        public static IHost Host => _Host 
+            ??= Programm.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Host.Services;
+
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var host = Host;
+            base.OnStartup(e);
+            await host.StartAsync();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using var host = Host;
+            base.OnExit(e);
+            await host.StopAsync();
+        }
     }
 }
---
+++ Programm.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace ScheduleMaster
+{
+    public static class Programm
+    {
+        public static void Main(string[] args)
+        {
+            var app =new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .ConfigureServices(App.ConfigureServices)
+        ;
+    }
+}
