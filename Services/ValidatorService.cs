8a3497c61a83422001d8f5f24e9bd0d5115096ab
Савицкий Даня <savitskiydanulo@gmail.com>
2020-11-29 02:04:59 +0200

    Сервис для проверки валидности объекта

---
+++ Services/ValidatorService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Windows.Documents;
+
+namespace ScheduleMaster.Services
+{
+    public static class ValidatorService
+    {
+        public static bool Validate<T>(T obj, out string error)
+        {
+            ValidationContext context = new ValidationContext(obj);
+            List<ValidationResult> results = new List<ValidationResult>();
+            bool valid = Validator.TryValidateObject(obj, context, results, true);
+            if (valid)
+            {
+                error = "";
+                return true;
+            }
+            else
+            {
+                error = results.ToString();
+                return false;
+            }
+        }
+    }
+}
